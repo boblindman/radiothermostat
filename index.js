356f3c92f96e073b55d90aaddd171c29731faafb
Bob Lindman <bob@lindman.us>
2022-02-10 12:35:06 -0500

    Updated registration info

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var request = require('request');
 module.exports = function(homebridge){
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-radiothermostat', 'RadioThermostat', Thermostat);
+  homebridge.registerAccessory('homebridge-radiothermostat-bob', 'RadioThermostat', Thermostat);
 };
 
 function fahrenheitToCelsius(temp) {
